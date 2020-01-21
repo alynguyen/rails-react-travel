00a96340337e29f9c7001d3502b42875f54c7459
Aly Nguyen <alynguyen@Alys-MacBook-Pro-2.local>
2020-01-20 19:44:02 -0800

    add cors to application

--- config/application.rb
+++ config/application.rb
@@ -33,3 +33,14 @@ module RailsReactTravel
     config.generators.system_tests = nil
   end
 end
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do 
+  allow do
+    origins 'https://rails-travelblogtest.herokuapp.com', 'localhost:3001' #change to domain with deployed
+  
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+end
