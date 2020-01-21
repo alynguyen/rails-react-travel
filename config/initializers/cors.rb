b3e0a967f4ce3ad36645b1e4afacb9514b2ef3f5
Aly Nguyen <alynguyen@Alys-MacBook-Pro-2.local>
2020-01-20 18:27:25 -0800

    fix error in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do 
   allow do
-    origins 'https://rails-travelblogtest.herokuapp.com' #change to domain with deployed
+    origins 'rails-travelblogtest.herokuapp.com', 'localhost:3001' #change to domain with deployed
   
     resource '*',
       headers: :any,
